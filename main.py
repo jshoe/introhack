3ee682b5250124e58c4af7ccb45f224379b14351
portdevil <portdevil@gmail.com>
2014-07-18 21:19:37 -0700

    okay basic program logic set up

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+def main():
+    user_name = input('\nHello, and welcome to the game.\nPlease enter your name:\n')
+    print("\nHello " + user_name + "!")
 
+    return
+
+def start_scene():
+    print("start text")
+
+def user_death():
+    print("You have died and failed. Goodbye.")
+    return
+
+def user_success():
+    print("You have beat the game! Congratulations! Enjoy the rest of your day.")
+
+main()
